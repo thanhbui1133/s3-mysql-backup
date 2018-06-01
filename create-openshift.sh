45c98a2e7c6339c574d863797ee52d516ae883f0
simbo1905 <simbo1905@60hertz.com>
2018-06-01 22:31:51 +0100

    appears to work

--- create-openshift.sh
+++ create-openshift.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 NAME=credentials ./create-file-secret.sh credentials
-NAME=config ./create-file-secret.sh config
+NAME=s3-mysql-backup ./create-env-secret.sh .env
 oc create -f openshift.yaml
