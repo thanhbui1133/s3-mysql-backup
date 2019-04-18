c4eddadcae1fd31eda3d467501909a00cb3c8788
thanh.bui <thanh.bui@niteco.se>
2019-04-18 17:31:17 +0700

    Update script new backup

--- mysql_s3_backup.sh
+++ mysql_s3_backup.sh
@@ -8,10 +8,13 @@ mysqluser="$MYSQL_USER"
 mysqlhost="$MYSQL_HOST"
 mysqlport="$MYSQL_PORT"
 bucket="$AWS_BUCKET"
+methods="$METHODS"
 
-stamp=`date +"%s_%A_%d_%B_%Y_%H%M"`
+#stamp=`date +"%s_%A_%d_%B_%Y_%H%M"`
+stampdate=`date +"%m_%d_%Y"`
+stamphour=`date +"%H_%M"`
 
-object="$bucket/$stamp/backup.sql"
+location="$stampdate/$stamphour.sql"
 
 /opt/rh/rh-mysql57/root/usr/bin/mysqldump -u $mysqluser -P $mysqlport -h $mysqlhost -u wordpress -p$mysqlpass $mysqlname > backup.sql;
 
@@ -22,9 +25,12 @@ else
   exit 1
 fi
 
-methods="$METHODS"
-
 IFS=","
+
+len=${#methods[@]}
+echo Found $len method:
+
+for (( i=0; i<$len; i++ )); do echo "- ${methods[$i]}\n" ; done
 
 read -ra methodsArr <<< "$methods"
 
@@ -36,28 +42,33 @@ for i in "${methodsArr[@]}"; do
         aws configure set aws_access_key_id "$AWS_ACCESS_KEY_ID"
         aws configure set aws_secret_access_key "$AWS_SECRET_ACCESS_KEY"
 
-        echo -e "  uploading..."
+        object="$bucket/$location"
+
+        echo -e " uploading..."
         aws s3 cp "backup.sql" "$object"
 
         if [ $? -eq 0 ]; then
-          echo Upload successful
+          echo "  Upload successful!"
         else
           echo FAILED Could not aws s3 cp "backup.sql" "$object"
           exit 2
         fi
         ;;
         "pvc") echo "Starting pvc backup..."
-        if [ ! -d "/data/backup/$stamp" ]; then
-            mkdir "/data/backup/$stamp"
+        if [ ! -d "/data/backup/$stampdate" ]; then
+            mkdir "/data/backup/$stampdate"
         fi
-        mv "backup.sql" "/data/backup/$stamp/backup.sql"
+        mv "backup.sql" "/data/backup/$stampdate/$stamphour.sql"
         if [ $? -eq 0 ]; then
-          echo Backup successful
+          echo " Backup successful"
         else
           echo FAILED Could not move "backup.sql" to specific folder
           exit 2
         fi
         ;;
+        *)
+		  echo "Method is none or invalid"
+		;;
     esac
 done
 
--- mysql_s3_restore.sh
+++ mysql_s3_restore.sh
@@ -1,10 +1,6 @@
 #!/bin/bash
 
 # Basic variables
-
-aws configure set aws_access_key_id "$AWS_ACCESS_KEY_ID"
-aws configure set aws_secret_access_key "$AWS_SECRET_ACCESS_KEY"
-
 mysqlpass="$MYSQL_PASSWORD"
 mysqlname="$MYSQL_NAME"
 mysqluser="$MYSQL_USER"
@@ -12,39 +8,94 @@ mysqlhost="$MYSQL_HOST"
 mysqlport="$MYSQL_PORT"
 siteurl="$SITE_URL"
 object="$AWS_BUCKET_PATH"
+location="$LOCATION"
+method="$METHODS"
+backuppath=""
 
-echo Getting object from "$object"
+case "$method" in
+    "s3") echo "Starting s3 restore..."
+        echo "Setup environment"
+        aws configure set aws_access_key_id "$AWS_ACCESS_KEY_ID"
+        aws configure set aws_secret_access_key "$AWS_SECRET_ACCESS_KEY"
 
-aws s3 cp "$object" "backup.sql"
+        echo Getting object from "$object"
 
-if [ $? -eq 0 ]; then
-    echo Download OK
-	mysqladmin -u $mysqluser -P $mysqlport -h $mysqlhost -p$mysqlpass --force drop $mysqlname
+        aws s3 cp "$object" "backup.sql"
 
-	mysqladmin -u $mysqluser -P $mysqlport -h $mysqlhost -p$mysqlpass --force create $mysqlname
-	
-	if [ $? -eq 0 ]; then
-		mysql -u $mysqluser -P $mysqlport -h $mysqlhost -p$mysqlpass --force $mysqlname < backup.sql &
-		
-		BACK_PID=$!
-		wait $BACK_PID
-		
-		# Delete
-		rm -f "backup.sql"
-		
-		sleep 2m
-		
-		if [ "$siteurl" != "" ]; then
-			mysql -u $mysqluser -P $mysqlport -h $mysqlhost -p$mysqlpass --force -D $mysqlname -e "UPDATE wp_options SET option_value = '$siteurl' where option_name = 'siteurl' or option_name = 'home'"
-		fi
-		echo Done
-	else 
-		echo FAILED to Import data;
-		exit 2
-	fi
- else
-    echo FAILED Could not get "$object"
-    exit 2
-fi
+        if [ $? -eq 0 ]; then
+          backuppath="backup.sql"
+          echo "  Download successful!"
+        else
+          echo FAILED Could not get from s3 "backup.sql" from "$object"
+          exit 2
+        fi
+        ;;
+    "pvc") echo "Starting pvc restore..."
+        if [ ! -f location ]; then
+            echo "Backup file not found! Finding lastest backup..."
+            cd /data/backup
+            now=`date +"%s"`
+            nearest=$now
+            nearestfolder=""
+            dirbackup=(`ls /data/backup`)
+            for i in "${dirbackup[@]}"; do
+                result=`echo "$i" | sed -r 's/[_]+/\//g'`
+                timestamptemp=`date -d $result +"%s"`
+                if [ $nearest -gt $timestamptemp ]; then
+                    nearest=$timestamptemp
+                    nearestfolder=$i
+                fi
+            done
+            cd $nearestfolder
+            filebackup=(`ls`)
+            nearest=${filebackup[0]}
+            for i in "${filebackup[@]}"; do
+                result=`echo "$i" | sed -r 's/[_]+/:/g'`
+                result=`echo "$result" | sed -r 's/[.sql]+//g'`
+                result+=":00"
+                timestamptemp=`date -d $result +"%s"`
+                if [ $timestamptemp -gt $nearest ]; then
+                    nearest=$timestamptemp
+                    nearestfile=$i
+                fi
+            done
+            backuppath="$nearestfolder/$nearestfile"
+        else
+            echo "Backup file found"
+            backuppath=$location
+        fi
+        echo
+        ;;
+    *) echo "Method is none or invalid"
+	    exit 2
+		;;
+    esac
+
+#if [ $? -eq 0 ]; then
+#    echo Download OK
+#	/opt/rh/rh-mysql57/root/usr/bin/mysqladmin -u $mysqluser -P $mysqlport -h $mysqlhost -p$mysqlpass --force drop $mysqlname
+#
+#	/opt/rh/rh-mysql57/root/usr/bin/mysqladmin -u $mysqluser -P $mysqlport -h $mysqlhost -p$mysqlpass --force create $mysqlname
+#
+#	if [ $? -eq 0 ]; then
+#		/opt/rh/rh-mysql57/root/usr/bin/mysql -u $mysqluser -P $mysqlport -h $mysqlhost -p$mysqlpass --force $mysqlname < backup.sql &
+#
+#		BACK_PID=$!
+#		wait $BACK_PID
+#
+#		# Delete
+#		rm -f "backup.sql"
+#
+#		sleep 2m
+#
+#		if [ "$siteurl" != "" ]; then
+#			/opt/rh/rh-mysql57/root/usr/bin/mysql -u $mysqluser -P $mysqlport -h $mysqlhost -p$mysqlpass --force -D $mysqlname -e "UPDATE wp_options SET option_value = '$siteurl' where option_name = 'siteurl' or option_name = 'home'"
+#		fi
+#		echo Done
+#	else
+#		echo FAILED to Import data;
+#		exit 3
+#	fi
+#fi
 
 
