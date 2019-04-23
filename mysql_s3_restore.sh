eb75fbee5d2e2741a394a9147700d94bf2b3ceb0
thanh.bui <thanh.bui@niteco.se>
2019-04-23 17:24:46 +0700

    Update restore database

--- mysql_s3_restore.sh
+++ mysql_s3_restore.sh
@@ -12,6 +12,33 @@ location="$LOCATION"
 method="$METHODS"
 backuppath=""
 
+find_lastest_bk() {
+    if [ -z "$1" ]
+    then
+        echo No variable found
+        exit 1
+    else
+        dirbackup=$1
+        nearest=""
+        for i in "${dirbackup[@]}"; do
+            IFS="_"
+            read -ra FILE_DATE <<< "$i"
+            result="${FILE_DATE[0]}/${FILE_DATE[1]}/${FILE_DATE[2]} ${FILE_DATE[3]}:${FILE_DATE[4]}:${FILE_DATE[5]}"
+            result=`echo "$result" | sed -r 's/[.sql]+//g'`
+            stamptemp=`date -d $result +"%s"`
+            if [ "$nearest" = "" ]; then
+                nearest=$stamptemp
+            fi
+            if [ "$stamptemp" -gt "$nearest" ]; then
+                nearest=$stamptemp
+                nearestfile=$i
+            fi
+            IFS=""
+        done
+        echo $nearestfile
+    fi
+}
+
 case "$method" in
     "s3") echo "Starting s3 restore..."
         echo "Setup environment"
@@ -26,47 +53,50 @@ case "$method" in
           backuppath="backup.sql"
           echo "  Download successful!"
         else
-          echo FAILED Could not get from s3 "backup.sql" from "$object"
-          exit 2
+            IFS="/"
+            read -ra ADDR <<< "$object"
+            len=${#ADDR[@]}
+            bucketstr="s3://"
+            if (( $(($len - 1)) <= 3 )); then
+                bucketstr+=${ADDR[2]}
+            else
+                for ((i=2;i<=$(($len - 2));i++)); do
+                    bucketstr+=${ADDR[$i]}/
+                done
+            fi
+            if aws s3 ls "$bucketstr" 2>&1 | grep -q 'NoSuchBucket\|AllAccessDisabled'; then
+                echo This bucket is not exist or access denied
+            else
+                echo FAILED Could not get from s3 "backup.sql" from "$object". Finding lastest backup at "$bucketstr"...
+                dirbackup=(`aws s3 ls "$bucketstr" | awk '{ print $4 }'`)
+                filteredFile=()
+                for i in ${dirbackup[@]}; do
+                    element=(`echo "$i" | awk "/[0-9]*_[0-9]*_[0-9]*_[0-9]*_[0-9]*_[0-9]*.sql/"`)
+                    filteredFile+=($element)
+                done
+                nearestfile="$(find_lastest_bk $filteredFile)"
+                echo Found lastest path: $bucketstr/$nearestfile
+                echo "Downloading..."
+                aws s3 cp "$bucketstr/$nearestfile" "backup.sql"
+                backuppath="backup.sql"
+            fi
         fi
         ;;
     "pvc") echo "Starting pvc restore..."
-        if [ ! -f location ]; then
+        if [ ! -f $location ]; then
             echo "Backup file not found! Finding lastest backup..."
-            cd /data/backup
+            cd /data/backup/$mysqlname
             now=`date +"%s"`
-            dirbackup=(`ls /data/backup`)
-            nearest=""
-            nearestfolder="${dirbackup[0]}"
-            for i in "${dirbackup[@]}"; do
-                result=`echo "$i" | sed -r 's/[_]+/\//g'`
-                timestamptemp=`date -d $result +"%s"`
-                if [ "$nearest" = "" ]; then
-                    nearest=$timestamptemp
-                fi
-                if [ "$timestamptemp" -gt "$nearest" ]; then
-                    nearest=$timestamptemp
-                    nearestfolder=$i
-                fi
-            done
-            cd $nearestfolder
-            filebackup=(`ls`)
-            nearest=""
-            echo $nearest
-            for i in "${filebackup[@]}"; do
-                result=`echo "$i" | sed -r 's/[_]+/:/g'`
-                result=`echo "$result" | sed -r 's/[.sql]+//g'`
-                result+=":00"
-                timestamptemp=`date -d $result +"%s"`
-                if [ "$nearest" = "" ]; then
-                    nearest=$timestamptemp
-                fi
-                if [ "$timestamptemp" -gt "$nearest" ]; then
-                    nearest=$timestamptemp
-                    nearestfile=$i
-                fi
-            done
-            backuppath="$nearestfolder/$nearestfile"
+            dirbackup=(`ls -d [0-9]*_[0-9]*_[0-9]*_[0-9]*_[0-9]*.sql`)
+            if (( ${#dirbackup[@]} > 0 )); then
+                nearest=""
+                nearestfile="$(find_lastest_bk $dirbackup)"
+                backuppath="/data/backup/$mysqlname/$nearestfile"
+                echo Found lastest path: $backuppath
+            else
+                echo "Can't found backup file"
+                backuppath=$location
+            fi
         else
             echo "Backup file found"
             backuppath=$location
@@ -77,32 +107,29 @@ case "$method" in
 	    exit 2
 		;;
     esac
-    echo $backuppath
-#if [ $? -eq 0 ]; then
-#    echo Download OK
-#	/opt/rh/rh-mysql57/root/usr/bin/mysqladmin -u $mysqluser -P $mysqlport -h $mysqlhost -p$mysqlpass --force drop $mysqlname
-#
-#	/opt/rh/rh-mysql57/root/usr/bin/mysqladmin -u $mysqluser -P $mysqlport -h $mysqlhost -p$mysqlpass --force create $mysqlname
-#
-#	if [ $? -eq 0 ]; then
-#		/opt/rh/rh-mysql57/root/usr/bin/mysql -u $mysqluser -P $mysqlport -h $mysqlhost -p$mysqlpass --force $mysqlname < backup.sql &
-#
-#		BACK_PID=$!
-#		wait $BACK_PID
-#
-#		# Delete
-#		rm -f "backup.sql"
-#
-#		sleep 2m
-#
-#		if [ "$siteurl" != "" ]; then
-#			/opt/rh/rh-mysql57/root/usr/bin/mysql -u $mysqluser -P $mysqlport -h $mysqlhost -p$mysqlpass --force -D $mysqlname -e "UPDATE wp_options SET option_value = '$siteurl' where option_name = 'siteurl' or option_name = 'home'"
-#		fi
-#		echo Done
-#	else
-#		echo FAILED to Import data;
-#		exit 3
-#	fi
-#fi
+if [ $? -eq 0 ]; then
+
+	/opt/rh/rh-mysql57/root/usr/bin/mysqladmin -u $mysqluser -P $mysqlport -h $mysqlhost -p$mysqlpass --force drop $mysqlname
+
+	/opt/rh/rh-mysql57/root/usr/bin/mysqladmin -u $mysqluser -P $mysqlport -h $mysqlhost -p$mysqlpass --force create $mysqlname
+
+	if [ $? -eq 0 ]; then
+		/opt/rh/rh-mysql57/root/usr/bin/mysql -u $mysqluser -P $mysqlport -h $mysqlhost -p$mysqlpass --force $mysqlname < $backuppath &
+
+		BACK_PID=$!
+		wait $BACK_PID
+
+
+		sleep 2m
+
+		if [ "$siteurl" != "" ] || [ "$siteurl" == "none" ]; then
+			/opt/rh/rh-mysql57/root/usr/bin/mysql -u $mysqluser -P $mysqlport -h $mysqlhost -p$mysqlpass --force -D $mysqlname -e "UPDATE wp_options SET option_value = '$siteurl' where option_name = 'siteurl' or option_name = 'home'"
+		fi
+		echo Done
+	else
+		echo FAILED to Import data;
+		exit 3
+	fi
+fi
 
 
