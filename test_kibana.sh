31d09c5fd00762f56f6e74d260f35c2b9219e047
thanh.bui <thanh.bui@niteco.se>
2019-05-24 17:18:48 +0700

    Update test

--- test_kibana.sh
+++ test_kibana.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
-echo "[error] This message for testing"
+echo "mysqldump: [Warning] Using a password on the command line interface can be insecure"
+
+echo "[Warning] Using a password on the command line interface can be insecure"
+
+echo "[Error] Using a password on the command line interface can be insecure"
 
 echo 123123
 
