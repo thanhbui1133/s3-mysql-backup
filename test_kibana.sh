56aba89a98f772c2642ca91f5095325215dc9d1a
thanh.bui <thanh.bui@niteco.se>
2019-05-24 17:31:58 +0700

    update test

--- test_kibana.sh
+++ test_kibana.sh
@@ -6,6 +6,7 @@ echo "[Warning] Using a password on the command line interface can be insecure"
 
 echo "[Error] Using a password on the command line interface can be insecure"
 
+exit 1
 echo 123123
 
 echo 123123
